07be57dc6392a6f1c8e5690a8c1d087f00536041
Jairo Vides <jairoeduardov@gmail.com>
2025-01-21 09:25:53 -0600

    Proyecto base  TodoService

---
+++ src/test/java/puntoxpress/com/todoservice/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package puntoxpress.com.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
